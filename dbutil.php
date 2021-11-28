ec67117f3b8be762c421fb8e43c2ce9a92403c64
Brooks <bwe3bx@virginia.edu>
2021-11-28 18:39:50 -0500

    changed extension from .py to .php

---
+++ dbutil.php
@@ -0,0 +1,20 @@
+<?php
+class DbUtil{
+	public static $loginUser = "cs4750jmi8fsa";
+	public static $loginPass = "cs4750mtgproject.a";
+	public static $host = "stardock.cs.virginia.edu"; // DB Host
+	public static $schema = "cs4750jmi8fs"; // DB Schema
+
+	public static function loginConnection(){
+		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
+
+		if($db->connect_errno){
+			echo("Could not connect to db");
+			$db->close();
+			exit();
+		}
+
+		return $db;
+	}
+
+}
