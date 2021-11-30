fc216c443e0bc2f1e74a34bf17f132d252ba842c
juliannecwalker <juliannecwalker@gmail.com>
2021-11-29 20:41:42 -0500

    Update playerDisplay2.php

--- playerDisplay2.php
+++ playerDisplay2.php
@@ -1,47 +1,54 @@
 <html>
-  <?php 
-    require('dbutil.php');
-  ?>
+    <?php 
+        require('dbutil.php');
+    ?>
 
-  <style>
-    .error_message {  color: crimson; font-style:italic; }       
-  </style>
+    <style>
+        .error_message {  color: crimson; font-style:italic; }       
+    </style>
 
-    <head>
-    
-    <title>Find Player</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="utf-8">
-    <meta name="author" content="Julianne Walker">
-    <meta name="description" content="Add Player">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-</head>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    session_start();
-    $_SESSION['name'] = $_POST['name'];
-    $name = $_SESSION['name'];
-    $query = "SELECT * FROM Players WHERE name=:name";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':name', $name);
-    $statement->execute();
-    $player_info = $statement->fetchAll();
-    $statement->closeCursor();
+        <head>
+        
+        <title>Find Player</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="author" content="Julianne Walker">
+        <meta name="description" content="Add Player">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        
+    </head>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        session_start();
+        $_SESSION['name'] = $_POST['name'];
+        $name = $_SESSION['name'];
+        $query = "SELECT * FROM Players WHERE name=:name";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':name', $name);
+        $statement->execute();
+        $player_info = $statement->fetchAll();
+        $statement->closeCursor();
 
-}
-?>
-<body>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Age</th> 
-            <th>Number</th>
-        </tr>
-        <tr>
-            <td><?php echo $player_info['name']; ?></td>
-            <td><?php echo $player_info['age']; ?></td> 
-            <td><?php echo $player_info['number']; ?></td>
-        </tr>
-    </table>
-</body>
+    }
+    ?>
+    <style>
+        table, th, td {
+            border:1px solid black;
+            width: 100%;
+        }
+    </style>
+    <body>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Age</th> 
+                <th>Number</th>
+            </tr>
+            <tr>
+                <td><?php echo $player_info['name']; ?></td>
+                <td><?php echo $player_info['age']; ?></td> 
+                <td><?php echo $player_info['number']; ?></td>
+            </tr>
+        </table>
+    </body>
+</html>
