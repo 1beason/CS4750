988937b6a3f76f38b6cc32c4e7466a8a2606a9ba
juliannecwalker <juliannecwalker@gmail.com>
2021-12-01 15:22:08 -0500

    Add header to view players

--- viewPlayers.php
+++ viewPlayers.php
@@ -9,11 +9,11 @@
 
     <head>
     
-    <title>Update Player</title>
+    <title>View Players</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <meta name="author" content="Julianne Walker">
-    <meta name="description" content="Update Player">
+    <meta name="description" content="View Players">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
     </head>
@@ -38,6 +38,7 @@
 
 ?>
 <body>
+        <h4><?php echo $user; echo "'s Players"; ?></h4>   
         <div class="table-responsive">
             <table id="playerTable" class="table table-striped table-bordered" style="width:100%">
                 <tr>
